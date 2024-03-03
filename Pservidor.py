d6c41cab31285784850aba0c0444b9405d22489b
Juan Pablo Ramirez <juanp@fincrimedynamics.com>
2024-03-02 20:51:21 -0500

    Adding function for having 1 pserver 1 pclient

--- Pservidor.py
+++ Pservidor.py
@@ -7,22 +7,51 @@ import Pservidor_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+userPort = ""
+file_list = []
+users = None
+
 class PeerServiceServicer(Pservidor_pb2_grpc.PeerServiceServicer):
 
+    def GetPortForUser(self, request, context):
+        user = request.user
+        print(userPort)
+        if user == users:
+            return Pservidor_pb2.PortResponse(port=userPort)
+        else:
+            return Pservidor_pb2.PortResponse(port="BadPort") 
+
+    def SetUserPort(self, request, context):
+        user = request.user
+        global users
+        if users == None:
+            users = user
+            return Pservidor_pb2.Response(success=True)
+        else:
+            return Pservidor_pb2.Response(success=False)
+        
     def GetFile(self, request, context):
         filename = request.filename
-        return Pservidor_pb2.FileResponse(content="Content of the file '{}'\n".format(filename))
+        if filename in file_list:
+            return Pservidor_pb2.FileResponse(content=filename)
+        else:
+            return Pservidor_pb2.FileResponse(content="File '{}' not found".format(filename))
 
     def CreateFile(self, request, context):
         filename = request.filename
-        content = request.content
+        global file_list
+        for file in filename:
+            file_list.append(file)
         return Pservidor_pb2.FileCreateResponse(success=True)
 
+
 def serve():
+    global userPort
     for port in range(50051, 50061):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         Pservidor_pb2_grpc.add_PeerServiceServicer_to_server(PeerServiceServicer(), server)
         try:
+            userPort = port
             server.add_insecure_port('[::]:{}'.format(port))
             server.start()
             print("The server is running on port {}...".format(port))
@@ -40,3 +69,4 @@ def serve():
 
 if __name__ == '__main__':
     serve()
+
